3986dded9ab511a6dbead7fb530e6b3d8a543c63
Monish Vassantrai <m.vassantrai@gmail.com>
2024-03-19 20:25:22 +0000

    refactor(tsconfig): `@/` alias should be `./src/*`

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { getAllCharacters } from '@/src/api/endpoints';
+import { getAllCharacters } from '@/api/endpoints';
 
 export default async function Home(): Promise<JSX.Element> {
   const characters = await getAllCharacters();

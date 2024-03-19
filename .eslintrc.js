c16ea4206c141a0b7c61e3825357c7ac704ba9db
Monish Vassantrai <m.vassantrai@gmail.com>
2024-03-19 19:15:20 +0000

    config: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: 12, sourceType: "module" },
+  plugins: ["@typescript-eslint", "react-hooks"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    semi: "error",
+    "padded-blocks": ["error", "never"],
+    quotes: ["error", "single", { avoidEscape: true }],
+    eqeqeq: "error",
+    "no-console": "error",
+    "no-case-declarations": "off",
+    indent: "off",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/explicit-member-accessibility": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" },
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};

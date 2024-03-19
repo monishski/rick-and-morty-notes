548ec86398291b22c9f6ef52886266d49be9b249
Monish Vassantrai <m.vassantrai@gmail.com>
2024-03-19 23:05:22 +0000

    feat: add SSR list item page

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { FaRobot, FaSkull } from 'react-icons/fa';
 import { HiOutlineStatusOnline } from 'react-icons/hi';
 import { RiAliensFill } from 'react-icons/ri';
@@ -9,24 +10,26 @@ import { Badge } from '@/components/atoms';
 
 import styles from './Card.module.css';
 
-type CardProps = Pick<Character, 'name' | 'image' | 'status' | 'gender' | 'species' | 'type'>;
+type CardProps = Pick<Character, 'id' | 'name' | 'image' | 'status' | 'gender' | 'species' | 'type'>;
 
-export const Card: FC<CardProps> = ({ name, image, status, gender, species, type }) => {
+export const Card: FC<CardProps> = ({ id, name, image, status, gender, species, type }) => {
   return (
-    <div className={styles.card}>
-      <div style={{ position: 'relative', width: '100%', aspectRatio: 1, borderRadius: 8, overflow: 'hidden' }}>
-        <Image src={image} alt={name} fill objectFit="cover" />
+    <Link href={`/${id}`}>
+      <div className={styles.card}>
+        <div style={{ position: 'relative', width: '100%', aspectRatio: 1, borderRadius: 8, overflow: 'hidden' }}>
+          <Image src={image} alt={name} fill objectFit="cover" />
+        </div>
+
+        <h2>{name}</h2>
+
+        <ul>
+          {/* TODO: clicking on badge should select filter */}
+          <Badge iconJsx={<HiOutlineStatusOnline />} title={status} subtitle="Status" />
+          <Badge iconJsx={<FaRobot />} title={gender} subtitle="Gender" />
+          <Badge iconJsx={<RiAliensFill />} title={species} subtitle="Species" />
+          {!!type && <Badge iconJsx={<FaSkull />} title={type} subtitle="Type" />}
+        </ul>
       </div>
-
-      <h2>{name}</h2>
-
-      <ul>
-        {/* TODO: clicking on badge should select filter */}
-        <Badge iconJsx={<HiOutlineStatusOnline />} title={status} subtitle="Status" />
-        <Badge iconJsx={<FaRobot />} title={gender} subtitle="Gender" />
-        <Badge iconJsx={<RiAliensFill />} title={species} subtitle="Species" />
-        {!!type && <Badge iconJsx={<FaSkull />} title={type} subtitle="Type" />}
-      </ul>
-    </div>
+    </Link>
   );
 };

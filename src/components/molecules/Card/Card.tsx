42d6e08b4ab952c474fd9286685d454f15a71775
Monish Vassantrai <m.vassantrai@gmail.com>
2024-03-20 17:14:46 +0000

    feat(CardSkeleton): add skeleton for character cards

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -11,13 +11,14 @@ import type { Character } from '@/api/types';
 import { Badge } from '@/components/atoms';
 
 import styles from './Card.module.css';
+import { CardSkeleton } from './CardSkeleton';
 
 type CardProps = Pick<Character, 'id' | 'name' | 'image' | 'status' | 'gender' | 'species' | 'type'> & {
   skeleton: boolean;
 };
 
 export const Card: FC<CardProps> = ({ id, name, image, status, gender, species, type, skeleton }) => {
-  if (skeleton) return <p key={id}>Skeleton</p>; //TEMP
+  if (skeleton) return <CardSkeleton />;
   return (
     <Link href={`/${id}`}>
       <div className={styles.card}>

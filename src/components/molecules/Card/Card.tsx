3a7f0a3d2d7f19cf298408c4d6a88de53bad6c6b
Monish Vassantrai <m.vassantrai@gmail.com>
2024-03-26 23:56:38 +0000

    feat(Card): add note badge to allow user to update note

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -3,11 +3,13 @@
 import type { FC } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { NotebookPen } from 'lucide-react';
 import { FaRobot, FaSkull } from 'react-icons/fa';
 import { HiOutlineStatusOnline } from 'react-icons/hi';
 import { RiAliensFill } from 'react-icons/ri';
 
-import { useAppSelector } from '@/store';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { setUpdateNoteModalMetadata, toggleUpdateModalVisible } from '@/features/notes';
 import { selectNoteByCharacterId } from '@/features/notes/store/selectors';
 import type { Character } from '@/api/types';
 import { Badge } from '@/components/atoms';
@@ -17,7 +19,15 @@ import styles from './Card.module.css';
 type CardProps = Pick<Character, 'id' | 'name' | 'image' | 'status' | 'gender' | 'species' | 'type'>;
 
 export const Card: FC<CardProps> = ({ id, name, image, status, gender, species, type }) => {
+  const dispatch = useAppDispatch();
+
   const note = useAppSelector((state) => selectNoteByCharacterId(state, id));
+
+  const handleNoteBadgeClick = (e): void => {
+    e.preventDefault();
+    dispatch(toggleUpdateModalVisible());
+    dispatch(setUpdateNoteModalMetadata({ id: note?.id }));
+  };
 
   return (
     <Link href={`/${id}`}>
@@ -33,8 +43,40 @@ export const Card: FC<CardProps> = ({ id, name, image, status, gender, species, 
           <Badge iconJsx={<RiAliensFill size={12} />} title={species} subtitle="Species" />
           {!!type && <Badge iconJsx={<FaSkull size={12} />} title={type} subtitle="Type" />}
         </ul>
-        ---
-        {!!note && <p>{note.title}</p>}
+        {!!note && (
+          <div
+            style={{
+              position: 'absolute',
+              top: '14px',
+              right: '14px',
+              backgroundColor: 'var(--rick-and-morty-theme-slate-5xlight)',
+              padding: '2px 6px',
+              borderRadius: '4px',
+              display: 'flex',
+              gap: '4px',
+
+              maxWidth: '120px',
+              overflow: 'hidden',
+            }}
+            onClick={handleNoteBadgeClick}
+          >
+            <div style={{ display: 'flex', gap: '4px', alignItems: 'center' }}>
+              <NotebookPen size={12} />
+              <span style={{ fontSize: '12px', fontWeight: 600 }}>Note</span>
+            </div>
+            <span
+              style={{
+                fontSize: '12px',
+                fontWeight: 300,
+                textWrap: 'nowrap',
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+              }}
+            >
+              · {note.title}
+            </span>
+          </div>
+        )}
       </div>
     </Link>
   );
